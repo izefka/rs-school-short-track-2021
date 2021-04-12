a57c903cf6e788cf8317de78af120fea73bc68b1
izefka <54077631+izefka@users.noreply.github.com>
2021-04-12 23:16:41 +0300

    fix: add new

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -2,14 +2,25 @@
  * Given a string, return its encoding version.
  *
  * @param {String} str
- * @return {String}
+ * @return {{}}
  *
  * @example
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const arrLength = arr.length;
+  let result = '';
+  for (let i = 0; i < arrLength; i++) {
+    if (arr[i] !== arr[i - 1] && arr[i] === arr[i + 1]) {
+      let counter = arr.lastIndexOf(arr[i]) - i + 1;
+      result += counter + arr[i];
+      counter = 0;
+    }
+    if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) result += arr[i];
+  }
+  return result;
 }
 
 module.exports = encodeLine;
