e762366d1ac4bf5bc338c8f0f42cd4586c45b18f
izefka <54077631+izefka@users.noreply.github.com>
2021-04-12 23:06:22 +0300

    add: new

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,17 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const result = names;
+  for (let i = 0; i < result.length; i++) {
+    let x = 1;
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[i] === result[j]) {
+        result[j] = `${result[j]}(${x++})`;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
