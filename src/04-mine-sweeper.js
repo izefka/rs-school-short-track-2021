3cca88d54aebded5c8ad6f14c5f1454821978b68
izefka <54077631+izefka@users.noreply.github.com>
2021-04-12 23:22:40 +0300

    fix: new

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -7,7 +7,7 @@
  * @param {Array<Array>} matrix
  * @return {Array<Array>}
  *
- * @example
+ *  @example
  * matrix = [
  *  [true, false, false],
  *  [false, true, false],
@@ -21,8 +21,30 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function toInt(xss, i, j) {
+  return (xss[i] && xss[i][j] && 1) || 0;
+}
+
+function sum(xss, i, j) {
+  let res = 0;
+  for (let n = -1; n <= 1; n++) {
+    for (let m = -1; m <= 1; m++) {
+      if (m !== 0 || n !== 0) res += toInt(xss, i + n, j + m);
+    }
+  }
+  return res;
+}
+
+function minesweeper(xss) {
+  const yss = [];
+  for (let i = 0; i < xss.length; i++) {
+    yss.push([]);
+    for (let j = 0; j < xss[i].length; j++) {
+      yss[i].push(sum(xss, i, j));
+    }
+  }
+  return yss;
 }
 
 module.exports = minesweeper;
